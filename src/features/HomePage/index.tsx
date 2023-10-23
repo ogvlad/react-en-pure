153bee0236a3602869f28df72b9b1a63813db1f8
Vlad Ogay <ogay.vlad@gmail.com>
2023-10-24 03:35:40 +0400

    Added "react-hook-form"

--- src/features/HomePage/index.tsx
+++ src/features/HomePage/index.tsx
@@ -1,11 +1,37 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import * as React from "react"
+import Button from "@mui/material/Button"
 import { Root } from "../../Root"
+import { useForm } from "react-hook-form"
+import { FormControl } from "@mui/base/FormControl"
 
 export const HomePage = () => {
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+  
   return (
     <Root>
-      <Button variant="contained">Hello world</Button>;
+      <FormControl defaultValue="" required>
+        {/*<CustomInput />*/}
+        {/*<ControlStateDisplay />*/}
+      </FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <section>
+          <textarea
+            placeholder="Enter text here..."
+            {...register("noteText", { required: true })}
+          />
+        </section>
+        <section>
+          <Button type={"submit"} variant="contained">Save</Button>;
+        </section>
+      </form>
     </Root>
   )
 }
