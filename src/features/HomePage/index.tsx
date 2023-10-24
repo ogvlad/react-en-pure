2a579146ee81c50ac5f0debeda68d3070a8df0b4
Vlad Ogay <ogay.vlad@gmail.com>
2023-10-24 05:32:03 +0400

    react-draggable

--- src/features/HomePage/index.tsx
+++ src/features/HomePage/index.tsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button"
 import { Root } from "../../Root"
 import { useForm } from "react-hook-form"
 import { FormControl } from "@mui/base/FormControl"
+import Draggable from "react-draggable"
 
 export const HomePage = () => {
   
@@ -17,6 +18,23 @@ export const HomePage = () => {
   
   return (
     <Root>
+      <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{ x: 0, y: 0 }}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        // onStart={this.handleStart}
+        // onDrag={this.handleDrag}
+        // onStop={this.handleStop}
+      >
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable>
+      
       <FormControl defaultValue="" required>
         {/*<CustomInput />*/}
         {/*<ControlStateDisplay />*/}
