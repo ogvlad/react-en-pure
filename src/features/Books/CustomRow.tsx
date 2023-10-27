512e9ebab3b8ecebaaec5d8edba8cded59268a85
Vlad Ogay <ogay.vlad@gmail.com>
2023-10-27 15:06:53 +0400

    Books: good

--- src/features/Books/CustomRow.tsx
+++ src/features/Books/CustomRow.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react"
 import { GridRow } from "@mui/x-data-grid"
 import { DraggableProvided, DraggableStateSnapshot, Draggable } from "react-beautiful-dnd"
+import { useItemsContext } from "../Notes/context"
 
 export const CustomRow = (props: any) => {
   
+  const { state, api } = useItemsContext()
   const { row } = props
   // console.debug("CustomRow", props)
   
@@ -12,32 +14,33 @@ export const CustomRow = (props: any) => {
   // if (!row.isSection && row.treeDataPath && row.treeDataPath.length === 1) return "Assessments-ItemRow"
   // if (row.isSection && row.treeDataPath && row.treeDataPath.length === 1) return "Assessments-ItemRow Assessments-SectionTitle"
   // if (row.treeDataPath && row.treeDataPath.length > 1) return null
+  if (row.pid !== "") return null
   
   return (
     <Draggable draggableId={row.id} index={props.index}>
       {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => {
         return (
-        <>
+        <div             ref={provided.innerRef}
+                         {...provided.draggableProps}
+                         {...provided.dragHandleProps}
+                         style={{ ...provided.draggableProps.style, position: 'static' }}
+        >
           <GridRow
             {...props}
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            style={{ ...provided.draggableProps.style, position: 'static' }}
           />
-          {/*{state.itemsList.map((item, index) => {*/}
-          {/*  // console.debug(item)*/}
-          {/*  if (item.pid !== row.id) return null*/}
+          {state.itemsList.map((item, index) => {
+            // console.debug(item)
+            if (item.pid !== row.id) return null
           
-          {/*  const childProps = {*/}
-          {/*    ...props,*/}
-          {/*    index,*/}
-          {/*    key: item.id,*/}
-          {/*    rowId: item.id,*/}
-          {/*  }*/}
-          {/*  return (<GridRow {...childProps} style={{ opacity }} />)*/}
-          {/*})}*/}
-        </>
+            const childProps = {
+              ...props,
+              index,
+              key: item.id,
+              rowId: item.id,
+            }
+            return (<GridRow {...childProps} style={{  }} />)
+          })}
+        </div>
         )
       }}
     </Draggable>
