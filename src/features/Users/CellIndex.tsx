59e5fd92815177128112470d702d36c0b650997a
Vlad Ogay <ogay.vlad@gmail.com>
2023-11-15 06:14:32 +0400

    CellIndex.tsx

--- src/features/Users/CellIndex.tsx
+++ src/features/Users/CellIndex.tsx
@@ -1,6 +1,11 @@
 import { SampleList } from "../../shared/data/SampleList"
+import { flattenCharacters } from "../../shared/data/flatten"
+import { useMemo } from "react"
 
 export const CellIndex = (params: any) => {
-  const rowIndex = SampleList.findIndex(row => row.id === params.id);
+  
+  const flatCharacters = useMemo(() => flattenCharacters(SampleList), [])
+  const rowIndex = flatCharacters.findIndex(row => row.id === params.id)
+  
   return (<div>{rowIndex}</div>)
 }
