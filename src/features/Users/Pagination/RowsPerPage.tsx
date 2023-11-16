f38dec00a952a4ff51b3b8239ab26ef42813e024
Vlad Ogay <ogay.vlad@gmail.com>
2023-11-16 11:54:05 +0400

    Pagination

--- src/features/Users/Pagination/RowsPerPage.tsx
+++ src/features/Users/Pagination/RowsPerPage.tsx
@@ -1,9 +1,35 @@
 import React from "react"
 import { styled } from "@mui/material"
-import { FormControl, InputLabel, MenuItem, Select, SelectProps } from "@mui/material"
-import IconButton from '@mui/material/IconButton';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { MenuItem, Select } from "@mui/material"
+import { SelectChangeEvent } from "@mui/material/Select/SelectInput"
+
+export const RowsPerPage = (props: RowsPerPageProps) => {
+  
+  const { options, onChange, defaultPageSize } = props
+  return (
+    <Container>
+      <LabelText>Rows per page:</LabelText>
+      <PageSizeSelector
+        variant={"standard"}
+        value={defaultPageSize}
+        onChange={onChange}
+        label=""
+      >
+        {options.map((value) => (
+          <MenuItem key={value} value={value}>
+            {value.toString()}
+          </MenuItem>
+        ))}
+      </PageSizeSelector>
+    </Container>
+  )
+}
+
+export class RowsPerPageProps {
+  options: number[] = [10, 30, 50, 100]
+  onChange?: (event: SelectChangeEvent<number>, child: React.ReactNode) => void = () => {}
+  defaultPageSize?: number = 10
+}
 
 const Container = styled("div")({
   display: "flex",
@@ -20,51 +46,3 @@ const PageSizeSelector = styled(Select)({
   marginLeft: "8px",
   marginRight: "32px",
 })
-
-const DisplayedRows = styled("p")({
-  display: "flex",
-  alignItems: "center",
-})
-
-const PaginationActions = styled("div")({
-  display: "flex",
-  alignItems: "center",
-  marginLeft: "20px",
-  marginRight: "0",
-})
-
-export const RowsPerPage = () => {
-  return (
-    <Container>
-      <LabelText>Rows per page:</LabelText>
-      <PageSizeSelector
-        variant={"standard"}
-        value={30}
-        onChange={() => {}}
-        label=""
-      >
-        {options.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.title}
-          </MenuItem>
-        ))}
-      </PageSizeSelector>
-      <DisplayedRows className={"MuiTablePagination-displayedRows"}>1&ndash;13 of 65</DisplayedRows>
-      <PaginationActions className={"MuiTablePagination-actions"}>
-        <IconButton aria-label="Go to previous page">
-          <KeyboardArrowLeftIcon />
-        </IconButton>
-        <IconButton aria-label="Go to next page">
-        <KeyboardArrowRightIcon />
-      </IconButton>
-      </PaginationActions>
-    </Container>
-  )
-}
-
-const options = [
-  { value: 10, title: "10" },
-  { value: 30, title: "30" },
-  { value: 50, title: "50" },
-  { value: 100, title: "100" },
-]
--- src/features/Users/Pagination/index.tsx
+++ src/features/Users/Pagination/index.tsx
@@ -1,4 +1,8 @@
 import React from "react"
+import { styled } from "@mui/material"
+import IconButton from "@mui/material/IconButton"
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft"
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight"
 import { PaginationRoot } from "./PaginationRoot"
 import { RowsPerPage } from "./RowsPerPage"
 import { Pages } from "./Pages"
@@ -7,8 +11,30 @@ export const Pagination = (props: any) => {
   console.debug(props)
   return (
     <PaginationRoot className={"MuiTablePagination-root Cirrus-pagination"}>
-      <RowsPerPage />
+      <RowsPerPage options={[5, 10, 15, 30, 50, 100]} defaultPageSize={5} />
+      <DisplayedRows className={"MuiTablePagination-displayedRows"}>1&ndash;50(89) of 65</DisplayedRows>
+      <PaginationActions className={"MuiTablePagination-actions"}>
+        <IconButton aria-label="Go to previous page">
+          <KeyboardArrowLeftIcon />
+        </IconButton>
+        <IconButton aria-label="Go to next page">
+          <KeyboardArrowRightIcon />
+        </IconButton>
+      </PaginationActions>
       <Pages />
     </PaginationRoot>
   )
 }
+
+
+const DisplayedRows = styled("p")({
+  display: "flex",
+  alignItems: "center",
+})
+
+const PaginationActions = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  marginLeft: "20px",
+  marginRight: "0",
+})
