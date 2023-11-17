c20a802fde961b0e40f521addc66089705efbf7f
Vlad Ogay <ogay.vlad@gmail.com>
2023-11-17 21:39:57 +0400

    Pagination: selected rows count

--- src/features/Users/TableFooter.tsx
+++ src/features/Users/TableFooter.tsx
@@ -1,7 +1,34 @@
 import React from "react"
 import { styled } from "@mui/material"
+import { GridRenderEditCellParams, useGridApiContext } from "@mui/x-data-grid"
+
 import { Pagination } from "./Pagination"
 import { PaginationProvider } from "./Pagination/context"
+
+export const TableFooter = (props: any) => {
+  // console.debug(props)
+  
+  const apiRef = useGridApiContext()
+  const selectedRows = apiRef.current?.getSelectedRows()
+  const selectedRowCount = selectedRows.size
+  
+  console.debug(selectedRows)
+  console.debug(selectedRowCount)
+
+  const { totalRows, options, initialPageSize } = props
+  
+  return (
+    <Container className={"MuiDataGrid-footerContainer"}>
+      
+      <div className={"MuiDataGrid-selectedRowCount"}>{selectedRowCount} row(s) selected</div>
+      
+      <PaginationProvider totalRows={totalRows} options={options} initialPageSize={initialPageSize}>
+        <Pagination />
+      </PaginationProvider>
+    
+    </Container>
+  )
+}
 
 const Container = styled("div")({
   display: "flex",
@@ -14,21 +41,3 @@ const Container = styled("div")({
     margin: "0px 16px",
   }
 })
-
-export const TableFooter = (props: any) => {
-  // console.debug(props)
-  
-  const { totalRows, options, initialPageSize } = props
-  
-  return (
-    <Container className={"MuiDataGrid-footerContainer"}>
-      
-      <div className={"MuiDataGrid-selectedRowCount"}>34 rows selected</div>
-      
-      <PaginationProvider totalRows={totalRows} options={options} initialPageSize={initialPageSize}>
-        <Pagination />
-      </PaginationProvider>
-      
-    </Container>
-  )
-}
